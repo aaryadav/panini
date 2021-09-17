ee7f8b4e912c33bbbbc3dbb4633dc45ad18ac3ac
aaryaman <aaryaxb1@gmail.com>
2021-09-17 21:23:14 +0530

    Added user registration view function.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import login_required, current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 from app.models import User
@@ -35,15 +35,19 @@ def login():
 def logout():
 	logout_user()
 	return redirect(url_for('home'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		user = User(username=form.username.data, email=form.email.data)
+		user.set_password(form.password.data)
+		db.session.add(user)
+		db.session.commit()
+		flash('Congratulations, you are now a registered user!')
+		return redirect(url_for('login'))
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
 	
-# @app.route('/signup')
-# @app.route('/register')
-# def signup():
-  # return render_template('signup.html', title = 'signup - panini')
-'''
-if __name__ == "__main__":  
-	app.run(
-		host='0.0.0.0',
-		# port=random.randint(2000, 9000)
-  )
-'''  
