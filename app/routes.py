17cad8aba8706df659c59740011f230c9a403552
aaryaman <aaryaxb1@gmail.com>
2021-09-17 20:07:34 +0530

    Added login and logout view functions.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, db
 from app.forms import LoginForm
-#from flask_login import login_required, current_user, login_user, logout_user
+from flask_login import login_required, current_user, login_user, logout_user
 from werkzeug.urls import url_parse
 from app.models import User
 #from app.forms import RegistrationForm, EditProfileForm
@@ -9,21 +9,41 @@ from datetime import datetime
 
 @app.route('/')
 @app.route('/home')
+@login_required
 def home():
   return render_template('base.html', title = 'panini')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-  form = LoginForm()
-  return render_template('login.html', title = 'login - panini', form=form)
+	if current_user.is_authenticated:
+		return redirect(url_for('index'))
+	form = LoginForm()
+	if form.validate_on_submit():
+		user = User.query.filter_by(username = form.username.data).first()
+		if user is None or not user.check_password(form.password.data):
+			flash('Invalid username or password')
+			return redirect(url_for('login'))
+		login_user(user, remember=form.remember_me.data)
+		next_page = request.args.get('next')
+		if not next_page or url_parse(next_page).netloc != '':
+			next_page = url_for('index')
+		return redirect(next_page)
+		#return redirect(url_for('index'))
+	return render_template('login.html', title = 'Sign In', form=form)
 
+@app.route('/logout')
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
+	
 # @app.route('/signup')
 # @app.route('/register')
 # def signup():
   # return render_template('signup.html', title = 'signup - panini')
-
+'''
 if __name__ == "__main__":  
 	app.run(
 		host='0.0.0.0',
 		# port=random.randint(2000, 9000)
   )
+'''  
